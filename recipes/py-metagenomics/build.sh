840b321661f645545a62c9ff836e5504aabe8eac
Marmaduke <jmeppley@gmail.com>
2018-01-26 12:00:22 -0800

    adding database files to share

--- recipes/py-metagenomics/build.sh
+++ recipes/py-metagenomics/build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 $PYTHON setup.py install 
+
+outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+mkdir -p $outdir
+rm -f $PREFIX/share/$PKG_NAME
+ln -s $PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM $PREFIX/share/$PKG_NAME
+cp -R ./databases $outdir/
